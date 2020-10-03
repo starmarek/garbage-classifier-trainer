e1982ac391f8292de3f12db898d8012de5220f96
Aleksander Pucher <pucher.aleksander@gmail.com>
2020-10-03 12:27:24 +0200

    add config handling

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
-from src.data_loader import DataLoader
-from src.model import ConvolutionModel
-from src.trainer import ModelTrainer
 from src.utils.config import process_config
 from src.utils.dirs import create_dirs
 from src.utils.args import get_args
+from src.utils import importer
+import sys
 
 
 def main():
     # capture the config path from the run arguments
-    # then process the json configuration file
+    # then process the json configuration fill
     try:
         args = get_args()
         config = process_config(args.config)
-    except Exception:
-        print("missing or invalid arguments")
-        exit(0)
 
-    # create the experiments dirs
-    create_dirs([config.callbacks.tensorboard_log_dir, config.callbacks.checkpoint_dir])
+        # create the experiments dirs
+        create_dirs(
+            [config.callbacks.tensorboard_log_dir, config.callbacks.checkpoint_dir]
+        )
 
-    print("Create the data generator.")
-    data_loader = DataLoader(config)
+        print("Create the data generator.")
+        data_loader = importer.get_class(config.data_loader.name)(config)
 
-    print("Create the model.")
-    model = ConvolutionModel(config)
+        print("Create the model.")
+        model = importer.get_class(config.model.name)(config)
 
-    print("Create the trainer")
-    trainer = ModelTrainer(model.model, data_loader.get_train_data(), config)
+        print("Create the trainer")
+        trainer = importer.get_class(config.trainer.name)(
+            model.model, data_loader.get_train_data(), config
+        )
 
-    print("Start training the model.")
-    trainer.train()
+        print("Start training the model.")
+        trainer.train()
+
+    except Exception as e:
+        print(e)
+        sys.exit(1)
 
 
 if __name__ == "__main__":
---
+++ src/utils/importer.py
@@ -0,0 +1,19 @@
+import importlib
+
+
+def get_class(cls):
+    """expects a string that can be imported as with a module.class name"""
+    module_name, class_name = cls.rsplit(".", 1)
+    module_name = "src." + module_name
+
+    try:
+        print("importing " + module_name)
+        somemodule = importlib.import_module(module_name)
+        print("getattr " + class_name)
+        cls_instance = getattr(somemodule, class_name)
+        print(cls_instance)
+    except Exception as err:
+        print("Creating directories error: {0}".format(err))
+        exit(-1)
+
+    return cls_instance
